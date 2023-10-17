6df89ba33fbb191b33ea209eef135dbeffb8d4db
Paulo Ricardo <1981928paulo@gmail.com>
2023-10-17 13:52:13 -0300

    Criação de Card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input()
+  gameCover: string = '';
+  @Input()
+  gameLabel: string = '';
+  @Input()
+  gameType: string = 'Digital PS4';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
